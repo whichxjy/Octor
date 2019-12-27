06094516f2fb557589f76cd92f4e7381a918fe16
whichxjy <whichxjy@gmail.com>
2019-12-27 21:02:04 +0800

    Add image picker

--- Octor/Main/HomeViewController.swift
+++ Octor/Main/HomeViewController.swift
@@ -4,13 +4,54 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class HomeViewController: UIViewController {
   
+  private var photoLibraryButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .blue
+    view.backgroundColor = .white
+    // add photo library button
+    addPhotoLibraryButton()
   }
   
+  private func addPhotoLibraryButton() {
+    photoLibraryButton = UIButton(frame: CGRect(x: 0, y: 100, width: 100, height: 50))
+    photoLibraryButton.center.x = self.view.center.x
+    photoLibraryButton.backgroundColor = .blue
+    photoLibraryButton.setTitle("从相册选择", for: .normal)
+    photoLibraryButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    self.view.addSubview(photoLibraryButton)
+  }
+  
+  @objc private func buttonAction(sender: UIButton!) {
+    let imagePicker = UIImagePickerController()
+    imagePicker.delegate = self
+    imagePicker.sourceType = .photoLibrary
+    imagePicker.mediaTypes = [kUTTypeImage as String]
+    self.present(imagePicker, animated: true)
+  }
+}
+
+// MARK: - UINavigationControllerDelegate
+extension HomeViewController: UINavigationControllerDelegate {
+  // empty
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension HomeViewController: UIImagePickerControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let selectedPhoto = info[.originalImage] as? UIImage else {
+      dismiss(animated: true)
+      return
+    }
+    
+    dismiss(animated: true) {
+      print(selectedPhoto.size)
+    }
+  }
 }
 
