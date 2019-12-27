8f9bbc0c6ace712833085c5f47b1adaa3ae55c84
whichxjy <whichxjy@gmail.com>
2019-12-27 18:12:52 +0800

    Init

---
+++ Octor/Main/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Created by @whichxjy.
+//  Copyright © 2019 @whichxjy. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+  
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '12.3'
+inhibit_all_warnings!
+
+target 'Octor' do
+  use_frameworks!
+  pod 'TesseractOCRiOS'
+end
