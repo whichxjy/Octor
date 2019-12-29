e666ba7ba1b3539daf1d76ca8dc07a75255b22c1
whichxjy <whichxjy@gmail.com>
2019-12-29 13:11:40 +0800

    Add preprocess function

--- Octor/ViewController/TextRecognizer.swift
+++ Octor/ViewController/TextRecognizer.swift
@@ -4,6 +4,7 @@
 //  
 
 import TesseractOCR
+import GPUImage
 
 class TextRecognizer {
   
@@ -17,7 +18,8 @@ class TextRecognizer {
   
   func recognize(_ image: UIImage) -> String? {
     let scaledImage = image.scale(maxDimension: 1000) ?? image
-    tesseract?.image = scaledImage
+    let preprocessedImage = scaledImage.preprocess() ?? scaledImage
+    tesseract?.image = preprocessedImage
     tesseract?.recognize()
     return tesseract?.recognizedText
   }
@@ -32,18 +34,25 @@ extension UIImage {
     var targetSize = CGSize(width: maxDimension, height: maxDimension)
     if size.width > size.height {
       // keep width to maxDimension and update height
-      targetSize.height = size.height / size.width * targetSize.width;
+      targetSize.height = size.height / size.width * targetSize.width
     }
     else {
       // keep height to maxDimension and update width
-      targetSize.width = size.width / size.height * targetSize.height;
+      targetSize.width = size.width / size.height * targetSize.height
     }
     // draw a new image
     UIGraphicsBeginImageContext(targetSize)
     draw(in: CGRect(origin: .zero, size: targetSize))
     let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
     UIGraphicsEndImageContext()
-    return scaledImage;
+    return scaledImage
+  }
+  
+  func preprocess() -> UIImage? {
+    let stillImageFilter = GPUImageAdaptiveThresholdFilter()
+    stillImageFilter.blurRadiusInPixels = 15.0
+    let filteredImage = stillImageFilter.image(byFilteringImage: self)
+    return filteredImage
   }
   
 }
